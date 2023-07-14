cada17a99bfe0b2f11e83f59fa8c0d02281aefe4
Janay Ma <janay.ma7@gmail.com>
2023-07-13 22:57:46 -0700

    get tweets from /tweets and format timestamp to show time since tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,31 +4,7 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-//fake tweets
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-]
+
 
 //takes in a tweet data structure and returns a snippet of html that will display the tweet on our webpage
 const createTweetElement = function(tweet) {
@@ -40,9 +16,9 @@ const createTweetElement = function(tweet) {
   const tweetContent = tweet.content.text
 
   //get date difference
-  const currDate = new Date().getTime();
+
   const tweetDate = tweet.created_at;
-  //const daysElapsed = Math.floor((currDate - tweetDate) / 86400000);
+  const timeAgo = timeago.format(tweetDate);
 
   const $tweet = `
     <article>
@@ -57,7 +33,7 @@ const createTweetElement = function(tweet) {
     <p>${tweetContent}</p>
 
     <footer>
-      <span>${tweetDate}</span>
+      <span>${timeAgo}</span>
       <span class="icon-bar">
         <i class="fa-solid fa-flag" style="color: #"></i>
         <i class="fa-solid fa-retweet"></i>
@@ -73,6 +49,7 @@ const createTweetElement = function(tweet) {
 
 //takes in an array of tweets and appends them to the tweet container in our index.html file
 const renderTweets = function (tweets){
+  console.log(tweets)
   for(tweet of tweets) {
     const $tweetElement = createTweetElement(tweet);
     $('.tweet-container').append($tweetElement);
@@ -80,6 +57,17 @@ const renderTweets = function (tweets){
 }
 
 
+
 $(document).ready(function (){
-  renderTweets(data)
+  
+  console.log('hello! from outside');
+  const loadTweets = function () {
+    const data = $.get('/tweets', function (data, status) {
+    renderTweets(data);
+    })
+    
+    
+  };
+  loadTweets();
+
 });
